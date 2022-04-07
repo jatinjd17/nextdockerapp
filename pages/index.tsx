879185e1bc031285f41cd49e578a183ffbce9fe6
jatinjd17 <jatindharmik@gmail.com>
2022-04-07 21:31:37 +0530

    3nd commmm

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welciiii<a href="https://nextjs.org">Next.jsjsjsjs!</a>
+          Wel123123<a href="https://nextjs.org">Next.jsjsjsjs!</a>
         </h1>
 
         <p className={styles.description}>
